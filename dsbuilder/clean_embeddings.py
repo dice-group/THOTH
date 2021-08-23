7d91f215bfa9fc23e5ddae8ef6a47c34ef939854
Nikit Srivastava <nikit91@gmail.com>
2021-08-23 18:14:31 +0200

    modifying clean embeddings

--- dsbuilder/clean_embeddings.py
+++ dsbuilder/clean_embeddings.py
@@ -5,7 +5,7 @@ import re
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-VECFILE, LANG = sys.argv[1:3]
+VECFILE, VOCAB, LANG = sys.argv[1:4]
 
 def clean(string):
     string = string.replace("http://dbpedia.org/ontology/", "dbo_")
@@ -17,10 +17,18 @@ def clean(string):
     string = string.lower()
     return string
 
-i = 0
+def_val = ' '.join([str(v) for v in [0.0]*500])
+vocab = list()
+with open(VOCAB) as f:
+    for line in f:
+        vocab.append(line)
+
 with open(VECFILE) as f:
     for line in f:
         sp = line.find(' ')
-        if ' ' in line[sp+1:-1]:
+        if ' ' in line[sp+1:-1] and line[:sp] in vocab:
+            vocab.remove(line[:sp])
             print clean(line[:sp]) + line[sp:-1]
-        i += 1
+    for item in vocab:
+        print item + ' ' + def_val
+
