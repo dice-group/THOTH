9f9238b6e264d70af65c6233ef5edd6691004feb
Nikit Srivastava <nikit91@gmail.com>
2021-09-17 12:46:35 +0200

    adding more cmd line args

--- train_own_emb.sh
+++ train_own_emb.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+SRC_LANG=$1
+TGT_LANG=$2
+DATA_DIR=$3
+OUT_DIR=$4
+NUM_TRAIN_STEPS=$5
+LEARNING_RATE=$6
+WARMUP_STEPS=$7
 cd nmt
-python -m nmt.nmt --src=$1 --tgt=$2 --vocab_prefix=../$3/vocab --dev_prefix=../$3/dev --test_prefix=../$3/test --train_prefix=../$3/train --out_dir=../$3_model --num_train_steps=$4 --attention=bahdanau --steps_per_stats=100 --num_layers=2 --num_units=500 --dropout=0.2 --metrics=accuracy --log_device_placement --learning_rate=0.5 --decay_scheme=luong10 --warmup_steps=20000 --warmup_scheme=t2t
+python -m nmt.nmt --src=$SRC_LANG --tgt=$TGT_LANG --vocab_prefix=../$DATA_DIR/vocab --dev_prefix=../$DATA_DIR/dev --test_prefix=../$DATA_DIR/test --train_prefix=../$DATA_DIR/train --out_dir=OUT_DIR --num_train_steps=$NUM_TRAIN_STEPS --attention=bahdanau --steps_per_stats=100 --num_layers=2 --num_units=500 --dropout=0.2 --metrics=accuracy --log_device_placement --learning_rate=$LEARNING_RATE --decay_scheme=luong10 --warmup_steps=$WARMUP_STEPS --warmup_scheme=t2t
 cd ..
