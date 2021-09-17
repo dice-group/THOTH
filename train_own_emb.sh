6d2cb65ed18820ce03f200b28540a968ae4cfe39
Nikit Srivastava <nikit91@gmail.com>
2021-09-17 12:18:11 +0200

    modifying lr

--- train_own_emb.sh
+++ train_own_emb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd nmt
-python -m nmt.nmt --src=$1 --tgt=$2 --vocab_prefix=../$3/vocab --dev_prefix=../$3/dev --test_prefix=../$3/test --train_prefix=../$3/train --out_dir=../$3_model --num_train_steps=$4 --attention=bahdanau --steps_per_stats=100 --num_layers=2 --num_units=500 --dropout=0.2 --metrics=accuracy --log_device_placement --learning_rate=0.2 --decay_scheme=luong10 --warmup_steps=2000 --warmup_scheme=t2t
+python -m nmt.nmt --src=$1 --tgt=$2 --vocab_prefix=../$3/vocab --dev_prefix=../$3/dev --test_prefix=../$3/test --train_prefix=../$3/train --out_dir=../$3_model --num_train_steps=$4 --attention=bahdanau --steps_per_stats=100 --num_layers=2 --num_units=500 --dropout=0.2 --metrics=accuracy --log_device_placement --learning_rate=0.5 --decay_scheme=luong10 --warmup_steps=20000 --warmup_scheme=t2t
 cd ..
