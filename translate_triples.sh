676ff7f5741031aae5d2c0eec382ac7e0dd2759c
Nikit Srivastava <nikit91@gmail.com>
2021-09-28 14:21:06 +0200

    fixing translate params

--- translate_triples.sh
+++ translate_triples.sh
@@ -6,7 +6,7 @@ IN_FILE=$3
 OUT_FILE=$4
 LOG_DIR=$5
 VOCAB_DIR=$6
-MODEL_DIR=$6
+MODEL_DIR=$7
 cd nmt
 python -m nmt.nmt  --vocab_prefix=$VOCAB_DIR --model_dir=$MODEL_DIR  --inference_input_file=$IN_FILE  --inference_output_file=$OUT_FILE --out_dir=$LOG_DIR --src=$SRC_LANG --tgt=$TGT_LANG
 echo "Translation complete."
